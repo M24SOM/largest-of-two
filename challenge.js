3ce40928c942a247929241d897bd92641f90792e
Mohamed Mahad Farah <mmfarah@proton.me>
2023-05-27 11:27:23 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+  console.log(`The largest number out of the two numbers is ${num1}`);
+} else {
+  console.log(`The largest number out of the two numbers is ${num2}`);
+}
